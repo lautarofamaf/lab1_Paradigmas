876ad89e8c9fe3887d30e459ed8751078916a891
Lautaro Famaf <lautaro.deco@mi.unc.edu.ar>
2025-03-17 18:03:34 -0300

    changes in Dibujo.hs

--- Dibujo.hs
+++ Dibujo.hs
@@ -11,22 +11,22 @@ data Dibujo a = Basica a
                 deriving (Show, Eq)
 
 rotar :: Dibujo a -> Dibujo a
-rotar d = Rotar d
+rotar = Rotar
 
 rotar45 :: Dibujo a -> Dibujo a
-rotar45 d = Rotar45 d
+rotar45 = Rot45
 
 espejar :: Dibujo a -> Dibujo a
-espejar d = Espejar d
+espejar = Espejar
 
 apilar :: Float -> Float -> Dibujo a -> Dibujo a -> Dibujo a
-apilar x y new d = Apilar x y new d
+apilar = Apilar
 
 juntar :: Float -> Float -> Dibujo a -> Dibujo a -> Dibujo a
-juntar x y new d = Apilar x y new d
+juntar = Apilar
 
-encimar :: Dibujo a -> Dibujo a
-encimar d = Encimar d
+encimar :: Dibujo a -> Dibujo a -> Dibujo a
+encimar = Encimar
 
 -- Composición n-veces de una función con sí misma.
 comp :: (a -> a) -> Int -> a -> a
@@ -35,10 +35,10 @@ comp f n x = f (comp f (n-1) x)
 
 -- Rotaciones de múltiplos de 90.
 r180 :: Dibujo a -> Dibujo a
-r180 d = comp rotar 2 d
+r180 = comp rotar 2 
 
 r270 :: Dibujo a -> Dibujo a
-r270 d = comp rotar 3 d
+r270 = comp rotar 3 
 
 -- Pone una figura sobre la otra, ambas ocupan el mismo espacio.
 (.-.) :: Dibujo a -> Dibujo a -> Dibujo a
@@ -68,7 +68,7 @@ ciclar d = cuarteto d (Rotar d) (r180 d) (r270 d)
 
 -- Transfomar un valor de tipo a como una Basica.
 pureDib :: a -> Dibujo a
-pureDib = Basica 
+pureDib = Basica
 -- map para nuestro lenguaje.
 mapDib :: (a -> b) -> Dibujo a -> Dibujo b
 mapDib f (Basica a) = Basica (f a)
@@ -80,8 +80,8 @@ mapDib f (Juntar x y d1 d2) = Juntar x y (mapDib f d1) (mapDib f d2)
 mapDib f (Encimar d1 d2) = Encimar (mapDib f d1) (mapDib f d2)
 -- Funcion de fold para Dibujos a
 foldDib :: (a -> b) -> (b -> b) -> (b -> b) -> (b -> b) ->
-       (Float -> Float -> b -> b -> b) -> 
-       (Float -> Float -> b -> b -> b) -> 
+       (Float -> Float -> b -> b -> b) ->
+       (Float -> Float -> b -> b -> b) ->
        (b -> b -> b) ->
        Dibujo a -> b
 foldDib fBas fRot fEsp fRot45 fAp fJun fEnc (Basica a) = fBas a
