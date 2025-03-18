d325d08909c555552033e2b920b519da9e1d5c36
Lautaro Famaf <lautaro.deco@mi.unc.edu.ar>
2025-03-18 19:04:21 -0300

    Complete errorfunctions

--- Pred.hs
+++ Pred.hs
@@ -12,8 +12,9 @@ type Pred a = a -> Bool
 -- segundo argumento con dicha figura.
 -- Por ejemplo, `cambiar (== Triangulo) (\x -> Rotar (Basica x))` rota
 -- todos los triángulos.
+-- A REVISAR FUNCION CAMBIAR ERROR DE TIPOS
 cambiar :: Pred a -> (a -> Dibujo a) -> Dibujo a -> Dibujo a
-cambiar p f dib = mapDib (\x-> if p x then f x else Basica x) dib
+cambiar p f = mapDib (\x-> if p x then f x else Basica x)
 
 -- Alguna básica satisface el predicado.
 anyDib :: Pred a -> Dibujo a -> Bool
@@ -34,17 +35,28 @@ esRot360 d = foldDib 0
 
 -- Hay 2 espejados seguidos.
 esFlip2 :: Pred (Dibujo a)
+esFlip2 d = foldDib 0
+                     (\x -> if x == 2 then 2 else 0)
+                     (\x -> if x < 2 then x+1 else 2)
+                     (\x -> if x == 2 then 2 else 0)
+                     (\_ _ x y -> if x == 2 || y == 2 then 2 else 0)
+                     (\_ _ x y -> if x == 2 || y == 2 then 2 else 0)
+                     (\x y -> if x == 2 || y == 2 then 2 else 0) d == 2
 
 data Superfluo = RotacionSuperflua | FlipSuperfluo
 
 ---- Chequea si el dibujo tiene una rotacion superflua
 errorRotacion :: Dibujo a -> [Superfluo]
-
+errorRotacion d = [RotacionSuperflua | esRot360 d]
 -- Chequea si el dibujo tiene un flip superfluo
 errorFlip :: Dibujo a -> [Superfluo]
+errorFlip d = [FlipSuperfluo | esFlip2 d]
 
 -- Aplica todos los chequeos y acumula todos los errores, y
 -- sólo devuelve la figura si no hubo ningún error.
 checkSuperfluo :: Dibujo a -> Either [Superfluo] (Dibujo a)
-
-
+checkSuperfluo d =
+  let errores = errorRotacion d ++ errorFlip d
+  in if null errores
+     then Right d
+     else Left errores
