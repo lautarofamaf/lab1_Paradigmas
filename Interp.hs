93fd418d3f0066b997c2742ed506706833b32061
TomasFT <tomasf.torres@mi.unc.edu.ar>
2025-03-27 18:30:38 -0300

    First attempt interp.hs
    Co-authored-by: Lautaro Famaf <lautaro.deco@mi.unc.edu.ar>

--- Interp.hs
+++ Interp.hs
@@ -17,15 +17,21 @@ mitad = (0.5 V.*)
 
 --interpreta el operador de rotacion
 interp_rotar :: ImagenFlotante -> ImagenFlotante
+interp_rotar d w h = (rotate -90) d+h h -w
 
 --interpreta el operador de espejar
 interp_espejar :: ImagenFlotante -> ImagenFlotante
+interp_espejar d w h = (scale -1 1) d+w -w h
+-- espejar(f)(d, w, h)
+-- f(d+w, -w, h)
 
 --interpreta el operador de rotacion 45
 interp_rotar45 :: ImagenFlotante -> ImagenFlotante
+interp_rotar45 = rotate 45 (d w h)
 
 --interpreta el operador de apilar
 interp_apilar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
+interp_apilar n m (d w h) (d1 w1 h1) =  
 
 --interpreta el operador de juntar
 interp_juntar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
@@ -36,4 +42,4 @@ interp_encimar :: ImagenFlotante -> ImagenFlotante -> ImagenFlotante
 --interpreta cualquier expresion del tipo Dibujo a
 --utilizar foldDib 
 interp :: Interpretacion a -> Dibujo a -> ImagenFlotante
-
+interp inte d = foldDib
